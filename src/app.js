46faaa7e07a1a52abcf6d270364255cd523b6250
Ralph Effting <ralfting@gmail.com>
2020-08-27 15:38:14 -0300

    Intgrate weather API to brings weathger in /weather route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const hbs = require("hbs");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = 4000;
@@ -22,11 +23,28 @@ app.get("/", (_req, res) => {
   });
 });
 
-app.get("/weather", (_req, res) => {
-  res.send({
-    forecast: "It's sowing",
-    location: "Philadeçphia",
-  });
+app.get("/weather", async (req, res) => {
+  const ACCESS_KEY = "e40d1cca7e7e7f464236024432f79237"; // TODO: extract to use env var
+  const qs = encodeURI(req.query.address);
+
+  const urlWeather = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${qs}`;
+
+  if (!req.query.address) {
+    return res.send("No ?address provided.");
+  }
+
+  try {
+    const response = await fetch(urlWeather);
+
+    const json = await response.json();
+
+    res.send({
+      forecast: json.current.weather_descriptions.join(", "),
+      location: json.location.name,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.get("/help", (_req, res) => {
